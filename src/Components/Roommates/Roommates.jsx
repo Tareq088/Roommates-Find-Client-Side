eb1c22f4b0d70830c25bfa78caf7a764ae38b1e7
Tareq1203088 <trahmanbpdb19@gmail.com>
2025-05-23 12:30:30 +0600

    slider part done

--- src/Components/Roommates/Roommates.jsx
+++ src/Components/Roommates/Roommates.jsx
@@ -14,11 +14,20 @@ const Roommates = () => {
     //     },[initialRoommates, roommates.availability]);
     // console.log(roommates);
     return (
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-5 w-11/12 mx-auto items-stretch py-10'>
-            {
-                initialRoommates.map(roommate => <RoommatesCard key={roommate._id} roommate={roommate}></RoommatesCard>)
-            }
+
+        <div>
+            <div className='flex flex-col items-center space-y-2'>
+                <h2 className='font-bold text-xl sm:text-2xl md:text-4xl text-purple-800'>Get Rommate</h2>
+                <p className='text-neutral-600'>A roommate is a person with whom one shares a living facility such as a room or dormitory except when being family or romantically involved. </p>
+            </div>
+
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-5 w-11/12 mx-auto items-stretch py-5'>
+                {
+                    initialRoommates.map(roommate => <RoommatesCard key={roommate._id} roommate={roommate}></RoommatesCard>)
+                }
+            </div>
         </div>
+
     );
 };
 
--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,9 +1,71 @@
 import React from 'react';
-
+import banner1Image from "../../assets/banner1.jpg"
+import banner2Image from "../../assets/banner2.jpg"
+import banner3Image from "../../assets/banner3.jpg"
 const Slider = () => {
     return (
-        <div>
-            slider
+        <div className='pb-5'>
+            <div className="carousel w-full h-[400px]">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img style={{backgroundImage: `linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,0.5)), url(${banner1Image})`}}
+                    className={`w-full object-contain bg-cover bg-top-center `}/>
+                    <div className='absolute top-1/3 flex gap-10 justify-between w-full px-10'>
+                        <div className="translate-y-1/2 transform ">
+                            <a href="#slide3" className="btn btn-circle">❮</a>                    
+                        </div>
+                        <div className=' space-y-2'>
+                            <h2 className='font-bold text-xl sm:text-3xl md:text-6xl text-blue-700'>Find Your Roommate</h2>
+                            <p className='text-white text-xs sm:text-base'> 10000+ rooms and flatmates available now across Bangladesh</p>
+                            <form>
+                                <input type="text" className="input" placeholder="search by location"/>
+                            </form>
+                        </div>
+                        <div className="translate-y-1/2 transform">
+                            <a href="#slide2" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img style={{backgroundImage: `linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,0.5)), url(${banner2Image})`}}
+                    className={`w-full object-contain bg-cover bg-top-center bg-radial from-black from-40% to-white`}/>
+                    <div className='absolute top-1/3 flex gap-10 justify-between w-full px-10'>
+                        <div className="translate-y-1/2 transform ">
+                            <a href="#slide1" className="btn btn-circle">❮</a>                    
+                        </div>
+                        <div className=' space-y-2'>
+                            <h2 className='font-bold text-xl sm:text-3xl md:text-6xl text-blue-700'>Find Your Roommate</h2>
+                            <p className='text-white text-xs sm:text-base'> 10000+ rooms and flatmates available now across Bangladesh</p>
+                            <form>
+                                <input type="text" className="input" placeholder="search by location"/>
+                            </form>
+                        </div>
+                        <div className="translate-y-1/2 transform">
+                            <a href="#slide3" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img style={{backgroundImage: `linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,0.5)), url(${banner3Image})`}}
+                    className={`w-full object-contain bg-cover bg-top-center bg-radial from-black from-40% to-white`}/>
+                    <div className='absolute top-1/3 flex gap-10 justify-between w-full px-10'>
+                        <div className="translate-y-1/2 transform ">
+                            <a href="#slide2" className="btn btn-circle">❮</a>                    
+                        </div>
+                        <div className=' space-y-2'>
+                            <h2 className='font-bold text-xl sm:text-3xl md:text-6xl text-blue-700'>Find Your Roommate</h2>
+                            <p className='text-white text-xs sm:text-base'> 10000+ rooms and flatmates available now across Bangladesh</p>
+                            <form>
+                                <input type="text" className="input" placeholder="search by location"/>
+                            </form>
+                        </div>
+                        <div className="translate-y-1/2 transform">
+                            <a href="#slide1" className="btn btn-circle">❯</a>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
         </div>
     );
 };
